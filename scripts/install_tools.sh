e2cfdd9a92d3934f64f569d9bc28c1236acb144b
Jiangqiu Shen <ivy22233qiu@live.com>
2023-01-25 19:49:55 -0500

    Update install_tools.sh

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -1,2 +1,2 @@
 yay;
-yay -S --needed just exa bat fd procs sd  dust starship ripgrep tokei hyperfine bottom tealdeer grex zoxide fzf sccache python python-pip wget
+yay -S --needed just exa bat fd procs sd  dust starship ripgrep tokei hyperfine bottom tealdeer grex zoxide fzf skim sccache python python-pip wget
